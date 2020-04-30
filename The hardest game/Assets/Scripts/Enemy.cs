9f05308bdd4207089f09d8fb27fc077214b5a2c3
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-04-30 16:23:41 +0300

    add level 2

--- The hardest game/Assets/Scripts/Enemy.cs
+++ The hardest game/Assets/Scripts/Enemy.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MovementType
+{
+    Vertical,
+    Horizontal
+}
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField] EnemyController enemyController;
+    [SerializeField] MovementType movementType;
     [SerializeField] float speed=5f;
 
     Rigidbody rb;
     Transform myTransform;
 
     float gameAreaLength;
+    float gameAreaHeight;
     float localScale;
-    float minPos;
-    float maxPos;
+    float minPosX;
+    float maxPosX;
+    float minPosZ;
+    float maxPosZ;
     Vector3 target;
 
     public Transform GameArea { get; set; }
@@ -24,10 +34,21 @@ public class Enemy : MonoBehaviour
 
         localScale = transform.localScale.x;
         gameAreaLength = GameArea.GetComponent<MeshRenderer>().bounds.size.x * 0.5f;
-        maxPos = gameAreaLength - localScale;
-        minPos = -maxPos;
+        gameAreaHeight = GameArea.GetComponent<MeshRenderer>().bounds.size.z * 0.5f;
+        maxPosX = gameAreaLength - localScale;
+        minPosX = -maxPosX;
+        maxPosZ = gameAreaHeight - localScale;
+        minPosZ = -maxPosZ;
 
-        target = new Vector3(maxPos, myTransform.position.y, myTransform.position.z);
+        if (movementType == MovementType.Horizontal)
+        {
+            target = new Vector3(maxPosX, myTransform.position.y, myTransform.position.z);
+        }
+        else
+        {
+            target = new Vector3(myTransform.position.x, myTransform.position.y, maxPosZ);
+        }
+        Debug.Log(target);
     }
 
     private void Update()
@@ -35,11 +56,19 @@ public class Enemy : MonoBehaviour
         if (enemyController.isGameOver)
             return;
 
-        float distance = Vector2.SqrMagnitude(target - myTransform.position);
+        float distance = Vector3.SqrMagnitude(target - myTransform.position);
 
         if (Mathf.Approximately(0, distance))
         {
-            target.x = target.x == maxPos ? minPos : maxPos;
+            if (movementType == MovementType.Horizontal)
+            {
+                target.x = target.x == maxPosX ? minPosX : maxPosX;
+            }
+            else
+            {
+                target.z = target.z == maxPosZ ? minPosZ : maxPosZ;
+            }
+            Debug.Log(target);
         }
     }
 
